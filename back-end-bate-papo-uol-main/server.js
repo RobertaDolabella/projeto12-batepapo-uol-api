50ce99a158903c3d5ed5fd1ad79352fb3ed4f24e
RobertaDolabella <dolabella.roberta@gmail.com>
2022-06-26 22:00:20 -0300

    Progresso: GET messages feito. Próximo passo: POST status

--- back-end-bate-papo-uol-main/server.js
+++ back-end-bate-papo-uol-main/server.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import cors from 'cors'
-import {Db, MongoClient, ObjectId} from 'mongodb'
+import { Db, MongoClient, ObjectId } from 'mongodb'
 import axios from 'axios'
 import Joi from 'joi'
 import dotenv from 'dotenv'
+import dayjs from 'dayjs'
 
 const server = express();
 
@@ -14,21 +15,22 @@ dotenv.config();
 
 const client = new MongoClient('mongodb://127.0.0.1:27017')
 let db;
-client.connect().then(()=>{
-    db=client.db('uol')
+client.connect().then(() => {
+    db = client.db('uol')
 })
-server.post('/participants', async (request, response)=>{
-    const newName = request.body
-   console.log(newName)
-   const loginArray = await db.collection('login').find().toArray()
 
-    if(!newName){
+let newName;
+server.post('/participants', async (request, response) => {
+    newName = request.body
+
+    const loginArray = await db.collection('login').find().toArray()
+
+    if (!newName) {
         response.sendStatus(422)
         return
     }
-    if(loginArray.some((login)=>login.name===newName.name)){
+    if (loginArray.some((login) => login.name === newName.name)) {
         response.sendStatus(409)
-        console.log("deu certo")
         return
     }
 
@@ -36,47 +38,98 @@ server.post('/participants', async (request, response)=>{
         name: newName.name,
         lastStatus: Date.now()
     }
-    db.collection('login').insertOne(dataLogin).then(()=>{
+    db.collection('login').insertOne(dataLogin).then(() => {
         response.sendStatus(201)
     })
-    const dataStatus ={
-        from: newName, 
-        to: 'Todos', 
-        text: 'entra na sala...', 
-        type: 'status', 
+    const dataStatus = {
+        from: newName,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
         time: new Date().getTime()
     }
-    db.collection('mensagem').insertOne(dataStatus)
+    db.collection('mensagens').insertOne(dataStatus)
 })
 
-server.get('/participants', async(request, response)=>{
+server.get('/participants', async (request, response) => {
     const loginArray = await db.collection('login').find().toArray()
 
     response.send(loginArray)
 })
 
-server.post('/messages', async (request, response)=>{
+server.post('/messages', async (request, response) => {
     const mensagem = request.body
-    const { user } = request.header
+    const { user } = request.headers
 
-    console.log(mensagem , user)
+    console.log(mensagem, user)
 
 
     const messageValidation = Joi.object({
-    from: Joi.string().required,
-    to: Joi.string().required(),
-    text: Joi.string().required(),
-    type:Joi.string().required()
-})
+        from: Joi.string().required,
+        to: Joi.string().required(),
+        text: Joi.string().required(),
+        type: Joi.string().required(),
+        time: Joi.required()
+    })
 
-    const dataMensagem =  {from: user,...mensagem}
+    const dataMensagem = { from:user , ...mensagem, time: dayjs().format('HH:MM:ss') }
 
     const validation = messageValidation.validate(dataMensagem)
 
-    const inputMessage = await db.collection("mensagem").insertOne({dataMensagem})
+    const inputMessage = await db.collection("mensagens").insertOne({ dataMensagem })
 
 
-    response.send(dataMensagem)
+    response.sendStatus(201)
 
+})
+
+server.get('/messages', async (request, response) => {
+
+    const { user } = request.headers
+
+    const findMessages = await db.collection("mensagens").find().toArray()
+    const filteredMessages = findMessages.filter((message) =>(message.dataMensagem.from === user||message.dataMensagem.to==="Todos"||message.dataMensagem.to===user))
+    
+    const limit = parseInt(request.query.limit)
+
+    if (limit) {
+        if (filteredMessages.length > limit) {
+            console.log("entrou no limit")
+            let splice = filteredMessages.length - limit
+            const limitedMessages = filteredMessages.splice(splice, limit)
+            response.send(limitedMessages)
+            return
+        }
+        else{
+            response.send(filteredMessages)
+        }
+    }
+        response.send(filteredMessages)
+})
+
+server.post('/status', async (request, response)=>{
+
+    const { user } = request.headers
+
+    const dataStatus = {
+        name: user,
+        lastStatus: Date.now()
+    }
+
+    const loginArray = await db.collection('login').find().toArray()
+
+    const statusList = await db.collection('status').find().toArray()
+
+    if(!loginArray.some(participants=>participants.name===user)){
+        response.sendStatus(404)
+        return
+    }
+
+    if(statusList.some(userOn=>userOn.name===user)){
+        db.collection('status').updateOne({"name":user}, { $set: { "lastStatus" : Date.now() } })
+    }
+    else{
+        db.collection('status').insertOne(dataStatus)
+    }
 })
 server.listen(5000)
